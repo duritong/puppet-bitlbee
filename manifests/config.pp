5b4a6a175f90b6ed7244c4ade54392d830e5cc66
TheCreeper <loxoko@gmail.com>
2014-11-09 11:26:43 +0000

    Changed notify service name

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,14 +5,14 @@ class bitlbee::config {
 
 	file { 'bitlbee.conf':
 
-		notify => Service["bitlbee"],
+		notify => Service[$service_name],
 		path => "${bitlbee::configdir}/bitlbee.conf",
 		content => template('bitlbee/bitlbee.conf.erb'),
 	}
 
 	file { 'motd.txt':
 
-		notify => Service["bitlbee"],
+		notify => Service[$service_name],
 		path => "${bitlbee::configdir}/motd.txt",
 		content => template('bitlbee/motd.txt.erb'),
 	}
