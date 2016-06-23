0280833b7969a90d3ceda9f2a7d466ed2911985e
Dave Simons <(none)>
2016-06-23 15:52:44 +0200

    add config options to config class

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,7 +1,26 @@
 # Class: bitlbee::config
 #
 # Class which configures the bitlbee service
-class bitlbee::config {
+class bitlbee::config (
+  $runmode         = $::bitlbee::runmode,
+  $user            = $::bitlbee::user,
+  $daemoninterface = $::bitlbee::daemoninterface,
+  $daemonport      = $::bitlbee::daemonport,
+  $clientinterface = $::bitlbee::clientinterface,
+  $authmode        = $::bitlbee::authmode,
+  $authpassword    = $::bitlbee::authpassword,
+  $operpassword    = $::bitlbee::operpassword,
+  $hostname        = $::bitlbee::hostname,
+  $motdfile        = $::bitlbee::motdfile,
+  $configdir       = $::bitlbee::configdir,
+  $pinginterval    = $::bitlbee::pinginterval,
+  $pingtimeout     = $::bitlbee::pingtimeout,
+  $proxy           = $::bitlbee::proxy,
+  $protocols       = $::bitlbee::protocols,
+  $ssl             = $::bitlbee::ssl,
+  $cafile          = $::bitlbee::cafile,
+  $private         = $::bitlbee::private,
+) {
 
   file { "${bitlbee::configdir}/bitlbee.conf":
     ensure  => file,
